00c9fc7e73be1a15f71a8e3cce433c462fd6ce76
Adamplenty23 <oladiplentypluto@gmail.com>
2023-09-07 17:18:53 -0700

    sizeof() operators

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,7 +14,7 @@ int main(void)
 	long long int _lint;
 
 	printf("size of a char: %lu byte(s)\n", sizeof(ch));
-	printf("size of an int: %lu bytes(s)\n", sizeof(itn));
+	printf("size of an int: %lu byte(s)\n", sizeof(itn));
 	printf("size of a long int: %lu byte(s)\n", sizeof(lint));
 	printf("size of a long long int: %lu byte(s)\n", sizeof(_lint));
 	printf("size of a float: %lu byte(s)\n", sizeof(flt));

4afca9ab97c91a28a97512463a07b5cedc04f259
Adamplenty23 <oladiplentypluto@gmail.com>
2023-09-08 18:08:38 -0700

    single digit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int _num = 0;
+
+	while (_num <= 9)
+	{
+	printf("%d", _num);
+	_num++;
+	}
+	putchar('\n');
+	return (0);
+}
+

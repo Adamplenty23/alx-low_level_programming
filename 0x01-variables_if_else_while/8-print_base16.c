84f84ef5fc2edd80684426dcc249faef219146cf
Adamplenty23 <oladiplentypluto@gmail.com>
2023-09-08 19:05:08 -0700

    hexadec

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char hexdigit;
+
+	for (hexdigit = '0'; hexdigit <= 'f'; hexdigit++)
+	{
+		if (hexdigit >= '0' && hexdigit <= '9')
+		{
+		       putchar(hexdigit);
+		}
+		else if (hexdigit >= 'a' && hexdigit <= 'f')
+		{
+			putchar(hexdigit);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+

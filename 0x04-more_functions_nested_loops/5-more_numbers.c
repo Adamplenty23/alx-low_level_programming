5ad2096e93f3a61dad37a3881dd504b0b188dd50
Adamplenty23 <oladiplentypluto@gmail.com>
2023-09-14 05:50:35 -0700

    were numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -13,7 +13,12 @@ void more_numbers(void)
 	for (a = 0; a <= 10; a++)
 	{
 		for (b = 0; b <= 14; b++)
-			_putchar(b + '0');
+		{
+			if (b >= 10)
+				_putchar('1');
+			if (b <= 13 || b == 14)
+				_putchar(b % 10 + '0');
+		}
 		_putchar('\n');
 	}
 }

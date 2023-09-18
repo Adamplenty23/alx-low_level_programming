22004e694e2d0d3f98f7cec561cfe3152f126d67
Adamplenty23 <oladiplentypluto@gmail.com>
2023-09-18 11:34:17 -0700

    resseter

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - a function that update the value of a pointer
+ *
+ * @n: the number to be updated
+ *
+ * Return: return nothing
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98; /*dereferencing the value of the variable to 98*/
+}

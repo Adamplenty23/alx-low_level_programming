8de412fafc7d5b40aa681f07ca06cbeb131281ad
Adamplenty23 <oladiplentypluto@gmail.com>
2023-09-18 21:10:22 -0700

    reversal

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,17 +1,22 @@
 #include "main.h"
 
 /**
- * _puts: A function that mimmicks puts and the content of the string
- * we are told to mimmick _puts function
+ * print_rev - A function that mimmicks puts and the content of the string
  *
- * @str: string to print in normal order
+ * @s: string to print in normal order
  *
  * Return: nothing since it is a void return type
  */
 
-void _puts(char *str)
+void print_rev(char *s)
 {
-	for (; *str != '\0'; str++) /*a loop condition to print the string*/
-		_putchar(*str);
+	int length, rev_conditn;
+
+	length = 0;
+
+	while (s[length] != '\0')
+		length++;
+	for (rev_conditn = length - 1; rev_conditn >= 0; rev_conditn--)
+		_putchar(s[rev_conditn]);
 	_putchar('\n');
 }

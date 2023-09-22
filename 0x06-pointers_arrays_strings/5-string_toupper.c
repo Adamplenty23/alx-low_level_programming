a5b2b5b996961eccd117cb2854517790c701c2e2
Adamplenty23 <oladiplentypluto@gmail.com>
2023-09-21 19:34:02 -0700

    lower to upper

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * string_toupper - change all lowercase to uppercase
+ *
+ * @n: pointer
+ *
+ * Return: n
+ */
+
+char *string_toupper(char *n)
+{
+	int a;
+
+	for (a = 0; n[a] != '\0'; a++)
+	{
+		if (n[a] >= 'a' && n[a] <= 'z')
+			n[a] = n[a] - 32;
+	}
+	return (n);
+}
+

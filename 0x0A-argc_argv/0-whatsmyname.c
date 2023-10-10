5f9da0f78cfebf07db81eb2f51106b4b566925dc
Adamplenty23 <oladiplentypluto@gmail.com>
2023-10-09 19:58:46 -0700

    tell me my fvcking name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - a program that prints its name
+ *
+ * @argv: array of the commands
+ *
+ * Return: 0 for success
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc >= 1)
+		printf("%s\n",argv[0]);
+	return (0);
+}

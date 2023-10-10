c3146db15477718e3a6e7a4af5fb3de515424ba3
Adamplenty23 <oladiplentypluto@gmail.com>
2023-10-09 20:07:40 -0700

    tell me my fvcking name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,4 +1,4 @@
-#include "main.h" 
+#include "main.h"
 
 /**
  * main - a program that prints its name
@@ -11,6 +11,6 @@
 int main(int argc, char *argv[])
 {
 	if (argc >= 1)
-		printf("%s\n",argv[0]);
+		printf("%s\n", argv[0]);
 	return (0);
 }

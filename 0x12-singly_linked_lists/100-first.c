d0dcb68e871f642687eb7f2627d830e9ff6c3221
Adamplenty23 <oladiplentypluto@gmail.com>
2023-10-19 20:13:39 -0700

    execute me before main

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+* myStartupFun - Apply the constructor attribute to myStartupFun() so that it
+* is executed before main() function
+*/
+
+void myStartupFun(void) __attribute__((constructor));
+
+/**
+* myStartupFun - a function that gets executed before main
+*/
+void myStartupFun(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
+

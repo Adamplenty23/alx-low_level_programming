fb95f1f525f2b804ce99b09b5e597c30300f8a9e
Adamplenty23 <oladiplentypluto@gmail.com>
2023-10-24 20:27:20 -0700

    print lenght

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * listint_len - a function that prints all the elements of a list
+ *
+ * @h: the head of the list
+ *
+ * Return: the lenght of the list
+ */
+
+size_t listint_len(const listint_t *h)
+{
+	size_t count;
+
+	count = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
+

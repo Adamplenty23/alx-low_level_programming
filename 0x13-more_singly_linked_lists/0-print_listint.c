605cefb65d8baee68b38786d18584d04c7b61c7c
Adamplenty23 <oladiplentypluto@gmail.com>
2023-10-24 20:03:47 -0700

    0-print list

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+
+/**
+ * print_listint - a function that prints all the elements of a list
+ *
+ * @h: the head of the list
+ *
+ * Return: the elements of the list
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	const listint_t *temp;
+	int count;
+
+	temp = h;
+	count = 0;
+
+	while (temp->next != NULL)
+	{
+		printf("%d\n", temp->n);
+		temp = temp->next;
+		count++;
+	}
+	printf("%d\n", temp->n);
+	count++;
+	return (count);
+}
+

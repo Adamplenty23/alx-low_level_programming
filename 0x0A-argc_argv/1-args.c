760d65a2a22584c8ee9ad546efefb7558b52d790
Adamplenty23 <oladiplentypluto@gmail.com>
2023-11-06 06:01:38 -0800

    1 argc

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - a program that prints its name
+ *
+ * @argc: number of command lines
+ * @argv: array of the commands
+ *
+ * Return: 0 for success
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc != 0)
+		printf("%d\n", argc);
+	return (0);
+}
